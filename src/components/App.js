b839ea3616ef1a7493a8876dbc03548f12d420d3
vchhavi68 <vchhavi68@gmail.com>
2026-02-06 14:31:54 +0530

    Add state management for button click count

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,11 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+   const [count,setCount] = useState(0)
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+    <button onClick={()=>setCount(count+1)}>Click me</button>
     </div>
   )
 }
